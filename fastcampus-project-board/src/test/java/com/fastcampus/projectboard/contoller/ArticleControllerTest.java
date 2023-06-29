41edea81bd8e96ffd37a82de223ff3e2f4c1586d
emhaki <mhmh779@naver.com>
2023-06-29 23:10:13 +0900

    #8 - 게시글 관련 뷰 테스트 작성
    어떤 경로로 뷰를 불러올지 테스트 명세

---
+++ fastcampus-project-board/src/test/java/com/fastcampus/projectboard/contoller/ArticleControllerTest.java
@@ -0,0 +1,50 @@
+package com.fastcampus.projectboard.contoller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() {
+        // "/articles"
+    }
+
+    @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleView_thenReturnsArticlesView() {
+
+
+        // "/articles/1"
+    }
+
+    @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleSearchView_thenReturnsArticlesSearchView() {
+
+
+        // "/articles/search"
+    }
+
+    @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleHashTagSearchView_thenReturnsArticlesHashtagSearchView() {
+
+
+        // "/articles/search-hashtag"
+    }
+}
