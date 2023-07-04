e45a2e16574d21c793db2b515192b39c3feaaf14
emhaki <mhmh779@naver.com>
2023-07-04 21:41:46 +0900

    #10 - querydsl 생성

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/dto/ArticleDto.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/dto/ArticleDto.java
@@ -1,14 +1,19 @@
 package com.fastcampus.projectboard.dto;
 
+import com.fastcampus.projectboard.domain.Article;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 public record ArticleDto(
-        String title,
-        String content,
-        String hashtag
 ) implements Serializable {
     public static ArticleDto of(LocalDateTime createdAt, String createdBy, String title, String content, String hashtag) {
-        return new ArticleDto(title, content, hashtag);
+        return new ArticleDto();
     }
+
+//    public static ArticleDto from(Article entity) {
+//        return new ArticleDto(
+//
+//        )
+//    }
 }
